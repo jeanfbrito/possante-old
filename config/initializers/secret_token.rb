9fe02a48c3a89ff38fb46da6fe1e86bab4da6a1c
Jean Brito <jeanbrito@Jeans-MacBook-Pro.local>
2013-10-07 19:36:47 -0300

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Possante::Application.config.secret_key_base = '341d0764bd922f487eb61fe36107320d282199691694e5da28c5627db6a8fcd34001b4b6044cfbcf11d2e24458782870a5a987618c2ecbf559befb4143f7125e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Possante::Application.config.session_store :cookie_store, key: '_possante_session'
