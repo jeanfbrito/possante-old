b631a32b9093df309ad00a37c485892a4c355df1
Jean Brito <jeanfbrito@gmail.com>
2013-11-13 22:04:34 -0200

    created the first seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+brand = Brand.create([{ name: 'Honda' }, { name: 'Toyota' }, { name: 'Ford' }, { name: 'Fiat' }])
+year = FabricationYear.create([{ number: '2000' }, { number: '2005' }, { number: '2010' }, { number: '2013' }])
+models = VehicleModel.create([ { 
+  name: 'Civic LX 16V 115cv 4p', brand_id: 1 },{
+  name: 'Corolla Xlei 110cv Aut.', brand_id: 2 },{
+  name: 'Focus bla bla.', brand_id: 3 },{
+  name: 'Uno Mille 1.0', brand_id: 4 
+  }])
+Vehicle.create( vehicle_model_id: 1, fabrication_year_id: 2, user_id: 1)
+VehicleModelsFabricationYear.create( vehicle_model_id: 1, fabrication_year_id: 1)
+VehicleModelsFabricationYear.create( vehicle_model_id: 1, fabrication_year_id: 2)
+VehicleModelsFabricationYear.create( vehicle_model_id: 1, fabrication_year_id: 3)
+
+  
