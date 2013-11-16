c707a925193a2deff869e5216c3c4e4192914f2f
Jean Brito <jeanfbrito@gmail.com>
2013-11-16 15:50:02 -0200

    now using pgsql to groupdate work, showing average_consumption in the chart

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131115220333) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "brands", force: true do |t|
     t.string   "name"
     t.string   "logo"
@@ -34,10 +37,10 @@ ActiveRecord::Schema.define(version: 20131115220333) do
     t.datetime "created_at"
   end
 
-  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "pages", force: true do |t|
     t.string   "title",      null: false
@@ -47,8 +50,8 @@ ActiveRecord::Schema.define(version: 20131115220333) do
     t.datetime "updated_at"
   end
 
-  add_index "pages", ["slug"], name: "index_pages_on_slug"
-  add_index "pages", ["title"], name: "index_pages_on_title"
+  add_index "pages", ["slug"], name: "index_pages_on_slug", using: :btree
+  add_index "pages", ["title"], name: "index_pages_on_title", using: :btree
 
   create_table "refuellings", force: true do |t|
     t.date     "date"
@@ -79,8 +82,8 @@ ActiveRecord::Schema.define(version: 20131115220333) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "vehicle_models", force: true do |t|
     t.integer  "brand_id"
