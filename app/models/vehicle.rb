21d0e9ad3bb3cebd35d6a6f4f1da43c88c1d00e8
Jean Brito <jeanfbrito@gmail.com>
2013-11-21 15:05:17 -0200

    add enumerate to vehicle status

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,13 @@
 class Vehicle < ActiveRecord::Base
-  extend FriendlyId
+  extend FriendlyId  
+  extend Enumerize
+
+  enumerize :status, :in => {
+    :ok => 0, 
+    :warning => 1, 
+    :danger => 2
+    }, default: :ok #, scope: true
+
   friendly_id :numberplate, use: :slugged
 
   has_many :refuellings
