2c3126c9a88602d66af4daa00c30f25f9068e66e
Jean Brito <jeanfbrito@gmail.com>
2013-11-23 12:30:44 -0200

    now create new refuelling works with inherited_resources, simple_form and friendly_id

--- app/controllers/refuellings_controller.rb
+++ app/controllers/refuellings_controller.rb
@@ -1,15 +1,48 @@
 class RefuellingsController < InheritedResources::Base
 
+  before_filter :find_vehicle
+
   def show
 
   end
 
   def index
-    @vehicle = Vehicle.friendly.find(params[:vehicle_id])
     @refuellings = @vehicle.refuellings.order('date desc')
 
     respond_to do |format|
       format.html
     end
   end
+
+  def new
+    @refuelling = @vehicle.refuellings.build
+    respond_to do |format|
+      format.html { render :layout => !request.xhr? }
+      format.json { render json: @refuelling }
+    end
+  end
+
+  def create
+    @refuelling = @vehicle.refuellings.build(resource_params)
+    if @refuelling.save
+      redirect_to [@vehicle, :refuellings], notice: 'Successfully created refuelling.'
+    else
+      render 'new'
+    end
+  end
+
+
+
+  private
+    # Using a private method to encapsulate the permissible parameters is just a good pattern
+    # since you'll be able to reuse the same permit list between create and update. Also, you
+    # can specialize this method with per-user checking of permissible attributes.
+    def resource_params
+      params.require(:refuelling).permit(:date, :km, :liter_price, :total_value)
+    end
+
+    def find_vehicle
+      @vehicle = Vehicle.friendly.find(params[:vehicle_id])
+    end
+
 end
--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,5 +1,5 @@
 class VehiclesController < InheritedResources::Base
-  #before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def show
     @vehicle = Vehicle.friendly.find(params[:id])
