4ee6bf5d4001020dadb740ec849c97be624a2a95
Jean Brito <jeanfbrito@gmail.com>
2013-11-23 12:56:14 -0200

    finish new refuelling form

--- app/controllers/refuellings_controller.rb
+++ app/controllers/refuellings_controller.rb
@@ -38,7 +38,7 @@ class RefuellingsController < InheritedResources::Base
     # since you'll be able to reuse the same permit list between create and update. Also, you
     # can specialize this method with per-user checking of permissible attributes.
     def resource_params
-      params.require(:refuelling).permit(:date, :km, :liter_price, :total_value)
+      params.require(:refuelling).permit(:date, :km, :liter_price, :total_value, :gas_station)
     end
 
     def find_vehicle
