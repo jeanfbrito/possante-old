048e7cb054030b58fc3ba44f10b0139af80f9085
Jean Brito <jeanfbrito@gmail.com>
2013-12-19 11:12:36 -0200

    modal works to load edit refuelling page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,23 @@ $("i").tooltip({
     'selector': '',
     'placement': 'top'
   });
+
+
+
+$(document).ready(function(){
+   $('.edit-refill-button').click(function() {
+    var url;
+    url = $(this).data('url');
+    return $.ajax({
+      url: url,
+      type: "GET",
+      success: function(data) {
+        return $('#editRefill .modal-body').html(data);
+      },
+      error: function() {
+        return $('#editRefill .modal-body').html("ERROR");
+      }
+    });
+  });   
+});
+
