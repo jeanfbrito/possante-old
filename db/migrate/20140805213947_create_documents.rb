cbebdc31ad60cfe407002f93047abade6f99b467
Jean Brito <jeanfbrito@gmail.com>
2014-08-05 18:43:20 -0300

    Create Documents migration

---
+++ db/migrate/20140805213947_create_documents.rb
@@ -0,0 +1,15 @@
+class CreateDocuments < ActiveRecord::Migration
+  def change
+    create_table :documents do |t|
+      t.integer :vehicle_id
+
+      t.string :title
+      t.text   :description
+
+      t.string  :file_uid
+      t.string  :file_name
+
+      t.timestamps
+    end
+  end
+end
